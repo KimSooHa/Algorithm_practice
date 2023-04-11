e264057afd68820f7a39361678331bf0e546568a
KimSooHa <tngk1001@naver.com>
2023-04-11 21:08:08 +0900

    [string] Practice6 - 문제 변경 / Practice7  - 문자열 나누기(프로그래머스 lv1) 문제 추

--- src/string/Practice6.java
+++ src/string/Practice6.java
@@ -1,34 +1,39 @@
 package string;
 
-import java.util.Arrays;
-
 public class Practice6 {
-	// 가장 가까운 같은 글자(프로그래머스) 
+	// 문자열 나누기 (프로그래머스 lv1) 
 	
-	public static int[] solution(String s) {
-        int[] answer = new int[s.length()];
-        
-        Arrays.fill(answer, -1);
-        
-        for(int i=1; i<s.length(); i++) {
-        	for(int j=0; j<i; j++) {
-        		if(s.charAt(i) == s.charAt(j))
-        			answer[i] = i-j;
-        	}
+	public static int solution(String s) {
+        int answer = 0;
+        int first = 0;
+        int rest = 0;
+        char x = s.charAt(0);
+        int length = s.length();
+        for(int i=0; i<length; i++) {
+            
+            if(first == rest) {
+                answer++;
+                x = s.charAt(i);
+                first = 0;
+                rest = 0;
+            }
+            
+            if(s.charAt(i) == x)
+                first++;
+            else 
+                rest++;
+            
         }
-        
         return answer;
     }
 	
 	public static void main(String[] args) {
 		
 		
-		String s = "foobar";
-		int[] answer = solution(s);
+		String s = "abracadabra";
+		int answer = solution(s);
 		
-		for(int i=0; i<answer.length; i++)
-			System.out.print(answer[i] + " ");
-		
+		System.out.println(answer);
 
 	}
 
