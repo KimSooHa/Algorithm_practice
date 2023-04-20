37c719363f106519df2485cd583553927a6295aa
KimSooHa <tngk1001@naver.com>
2023-04-21 06:09:13 +0900

    [string] Practice11 - 행렬의 덧셈(프로그래머스) 문제 추가

---
+++ src/string/Practice11.java
@@ -0,0 +1,27 @@
+package string;
+
+public class Practice11 {
+	// 행렬의 덧셈 (프로그래머스) 
+	    
+	public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for(int i=0; i<arr1.length; i++) {
+            for(int j=0; j<arr1[0].length; j++) 
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+        }
+        return answer;
+    }
+	
+	
+	public static void main(String[] args) {
+		int[][] arr1 = {{1,2},{2,3}};
+		int[][] arr2 = {{3,4},{5,6}};	
+		int[][] answer = solution(arr1, arr2);
+		for(int i=0; i<answer.length; i++) {
+			for(int j=0; j<answer[0].length; j++) 
+				System.out.print(answer[i][j] + " ");
+				
+			System.out.println();
+		}
+	}
+}
