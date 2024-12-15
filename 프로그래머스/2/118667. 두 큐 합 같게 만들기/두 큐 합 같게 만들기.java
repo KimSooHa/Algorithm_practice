e574574174503a58eefc5d9696c057605fd6d627
sooohk <81688625+KimSooHa@users.noreply.github.com>
2024-12-16 00:45:19 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 100.55 ms, Memory: 162 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] queue1, int[] queue2) {
+        long answer = -1;
+        
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        long q1t = 0;
+        long q2t = 0;
+        for(int i=0; i<queue1.length; i++) {
+            q1.offer(queue1[i]);
+            q2.offer(queue2[i]);
+            q1t += queue1[i];
+            q2t += queue2[i];
+        }
+        
+        int work = 0;
+        int cnt = 0;
+        while(q1t != q2t) {
+            if(cnt >= queue1.length * 3 - 3)
+                return answer;
+            
+            if(q1t > q2t) {
+                q2t += q1.peek();
+                q1t -= q1.peek();
+                q2.offer(q1.poll());
+                work++;
+            } else {
+                q1t += q2.peek();
+                q2t -= q2.peek();
+                q1.offer(q2.poll());
+                work++;
+            }
+            cnt ++;
+        }
+        
+        answer = work;
+        return answer;
+    }
+}
