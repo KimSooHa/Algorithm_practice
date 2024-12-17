aca8b59e9b1f16aa1a562cc05e34080c28eb96f6
sooohk <81688625+KimSooHa@users.noreply.github.com>
2024-12-18 01:35:50 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.87 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> basket = new Stack<>();
+        
+        for(int j=0; j<moves.length; j++) {
+            int move = moves[j] - 1;
+            for(int i=0; i<board.length; i++) {
+                if(board[i][move] != 0) {
+                    if(basket.size() > 0 && basket.peek() == board[i][move]) {
+                        basket.pop();
+                        answer += 2;
+                    } else    
+                        basket.push(board[i][move]);
+                    
+                    board[i][move] = 0;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
