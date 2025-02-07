d66614a8bde4e7a8c01b8f71cb0e470122290ef8
sooohk <81688625+KimSooHa@users.noreply.github.com>
2025-02-07 22:10:37 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 5.21 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/README.md
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 10번 / 데이터 분석 - 250121 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250121) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250121?language=java) 
 
 ### 성능 요약
 
-메모리: 80.4 MB, 시간: 7.75 ms
+메모리: 79.9 MB, 시간: 5.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 07일 22:06:10
+2025년 02월 07일 22:10:33
 
 ### 문제 설명
 
--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,80 +1,32 @@
 import java.util.*;
 
 class Solution {
-    
-    static String type;
-	
-	static class Data implements Comparable<Data> {
-		
-		private int code;
-		private int date;
-		private int maximum;
-		private int remain;
-		
-		public Data(int code, int date, int maximum, int remain) {
-			this.code = code;
-			this.date = date;
-			this.maximum = maximum;
-			this.remain = remain;
-		}
-		
-		public int getCode() {
-			return this.code;
-		}
-		
-		public int getDate() {
-			return this.date;
-		}
-		
-		public int getMaximum() {
-			return this.maximum;
-		}
-		
-		public int getRemain() {
-			return this.remain;
-		}
-		
-		
-		@Override
-		public int compareTo(Data o) {
-			if(type.equals("code"))
-				return this.code - o.code;
-			else if (type.equals("date"))
-				return this.date - o.date;
-			else if(type.equals("maximum"))
-				return this.maximum - o.maximum;
-			
-			return this.remain - o.remain;
-		}
-		
-	}
 	
 	public static int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
         String[] dataType = {"code", "date", "maximum", "remain"};
-        type = sort_by;
         int idx = 0;
+        int sortIdx = 0;
         for(int i=0; i<dataType.length; i++) {
         	if(ext.equals(dataType[i]))
         		idx = i;
+        	if(sort_by.equals(dataType[i]))
+        		sortIdx = i;
         }
         
-        List<Data> list = new ArrayList<>();
+        final int index = sortIdx;
+        List<int[]> list = new ArrayList<>();
         
         for(int i=0; i<data.length; i++) {
         	if(data[i][idx] < val_ext)
-       		list.add(new Data(data[i][0], data[i][1], data[i][2], data[i][3]));
+        		list.add(data[i]);
         }
         
-        Collections.sort(list);
+        Collections.sort(list, (o1, o2) -> o1[index] - o2[index]);
         
-        int[][] answer = new int[list.size()][4];        
-        
-       for(int i=0; i<list.size(); i++) {
-       	answer[i][0] = list.get(i).getCode();
-       	answer[i][1] = list.get(i).getDate();
-       	answer[i][2] = list.get(i).getMaximum();
-       	answer[i][3] = list.get(i).getRemain();
-       }
+        int[][] answer = new int[list.size()][4];
+        for(int i=0; i<answer.length; i++) {
+        	answer[i] = list.get(i);
+        }
         
         return answer;
     }
